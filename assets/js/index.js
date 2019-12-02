ce05544613dddc6522fea3f6eb8322cf6d9fd1c9
Bgh0602 <57307366+Bgh0602@users.noreply.github.com>
2019-12-02 17:24:42 +0900

    ADD +prompt

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,16 @@
-var x = -300;
-while (x  < 1)
-{document.write(x)
-document.write("<br>")
-  x=x+1;
+function print(s) {
+  document.write(s);
+}
+
+var n = +prompt("HOW MANY?");
+var a = 1 ;
+var b;
+while (a < n+1) {
+  b = 1;
+  while (b < a+1) {
+      print("*")
+      b = b + 1;
+  }
+  print("<br>")
+ a = a + 1;
 }
