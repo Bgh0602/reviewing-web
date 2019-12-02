60ff408651de45ee6eee8ed67f64cb34639c0492
Bgh0602 <57307366+Bgh0602@users.noreply.github.com>
2019-12-02 13:36:13 +0900

    ADD if/else

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,6 @@
-var q = "네 이름이 뭐니?"
-var answer = prompt(q)
-alert(answer)
-
-var x = "40 + 30";
-var y = 40 + 30;
+var answer=prompt("황대연은 학교를 나올까?")
+if (answer == "나와라"){
+  alert("잘왔어요")
+} else {
+  alert("양아치")
+}
