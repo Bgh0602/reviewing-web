0484952cc8a679a3ae0b725e6d500a7de0d9eaa7
Bgh0602 <57307366+Bgh0602@users.noreply.github.com>
2019-12-03 12:30:49 +0900

    ADD user interaction

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,13 @@
-function print(s) {
-  document.write(s);
+var q = confirm("Are we in school")
+if (q) {
+  alert("you are good")
+} else {
+  alert("양아치")
 }
 
-var n = +prompt("HOW MANY?");
-var a = 1 ;
-var b;
-while (a < n+1) {
-  b = 1;
-  while (b < a+1) {
-      print("*")
-      b = b + 1;
-  }
-  print("<br>")
- a = a + 1;
+var q = prompt("Why are you in school?")
+if (q == "I'm a student") {
+  alert("Good")
+} else {
+  alert("Think more")
 }
